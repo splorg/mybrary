7d89194598ca47f054cd4f4880598246f7b131d2
Gabriel Lima <gabriel.limabs02@gmail.com>
2022-09-08 15:12:06 -0300

    upload de imagem de capa dos livros

--- routes/books.js
+++ routes/books.js
@@ -1,18 +1,8 @@
 const express = require('express')
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
+const router = express.Router()
 const Book = require('../models/book')
 const Author = require('../models/author')
-const router = express.Router()
-const uploadPath = path.join('public', Book.coverImageBasePath)
-const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
-const upload = multer({
-  dest: uploadPath,
-  fileFilter: (req, file, callback) => {
-    callback(null, imageMimeTypes.includes(file.mimetype))
-  }
-})
+const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
 
 // all books route
 router.get('/', async (req, res) => {
@@ -43,50 +33,45 @@ router.get('/new', async (req, res) => {
 })
 
 // create book route
-router.post('/', upload.single('cover'), async (req, res) => {
-  const fileName = req.file != null ? req.file.filename : null
+router.post('/', async (req, res) => {
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
     publishDate: new Date(req.body.publishDate),
     pageCount: req.body.pageCount,
-    coverImageName: fileName,
     description: req.body.description
   })
+  saveCover(book, req.body.cover)
 
   try {
     const newBook = await book.save()
     // res.redirect(`books/${newBook.id}`)
     res.redirect(`books`)
   } catch {
-    if (book.coverImageName != null) {
-      removeBookCover(book.coverImageName)
-    }
     renderNewPage(res, book, true)
   }
 })
-
-function removeBookCover(fileName) {
-  fs.unlink(path.join(uploadPath, fileName), err => {
-    if (err) {
-      console.log(err)
-    }
-  })
-}
 
 async function renderNewPage(res, book, hasError = false) {
   try {
     const authors = await Author.find({})
     const params = {
-        authors: authors,
-        book: book
+      authors: authors,
+      book: book
     }
-    if (hasError) {
-      params.errorMessage = 'Error creating book'
-    }
+    if (hasError) params.errorMessage = 'Error Creating Book'
     res.render('books/new', params)
   } catch {
     res.redirect('/books')
+  }
+}
+
+function saveCover(book, coverEncoded) {
+  if (coverEncoded == null) return
+  const cover = JSON.parse(coverEncoded)
+  if (cover != null && imageMimeTypes.includes(cover.type)) {
+    book.coverImage = new Buffer.from(cover.data, 'base64')
+    book.coverImageType = cover.type
   }
 }
 
