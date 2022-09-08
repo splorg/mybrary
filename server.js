21a3c5ff4ad945c2fa9db55f58cd839185b5c597
Gabriel Lima <gabriel.limabs02@gmail.com>
2022-09-08 14:09:49 -0300

    indice de livros e criar novo livro

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express()
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', `${__dirname}/views`)
@@ -28,5 +29,6 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
