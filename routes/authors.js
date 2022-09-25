cbecb17d5be0a00016731099e0eaab8b7ca32f5d
Gabriel Lima <gabriel.limabs02@gmail.com>
2022-09-25 02:05:58 -0300

    rotas dos autores finalizadas

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Author = require('../models/author')
+const Book = require('../models/book')
 const router = express.Router()
 
 // all authors route
@@ -32,13 +33,70 @@ router.post('/', async (req, res) => {
 
   try {
     const newAuthor = await author.save()
-    // res.redirect(`authors/${newAuthor.id}`)
-    res.redirect('authors')
+    res.redirect(`authors/${newAuthor.id}`)
   } catch {
     res.render('authors/new', {
       author: author,
       errorMessage: 'Error creating author'
     })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    const books = await Book.find({ author: author.id }).limit(6).exec()
+    res.render('authors/show', {
+      author: author,
+      booksByAuthor: books
+    })
+  } catch (err) {
+    res.redirect('/')
+    console.log(err)
+  }
+})
+
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    res.render('authors/edit', { author: author })
+  } catch (err) {
+    res.redirect('/authors')
+    console.log(err)
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    author.name = req.body.name
+    await author.save()
+    res.redirect(`/authors/${author.id}`)
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.render('authors/edit', {
+        author: author,
+        errorMessage: 'Error updating the author'
+      })
+    }
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    await author.remove()
+    res.redirect(`/authors`)
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.redirect(`/authors/${author.id}`)
+    }
   }
 })
 
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const app = express()
 
@@ -19,6 +20,7 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(methodOverride('_method'))
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true
